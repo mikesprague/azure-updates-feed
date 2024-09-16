5eeb5a248e4c1cdc66e979ddc58cdafdc44a294d
Michael Sprague <mikesprague@gmail.com>
2024-09-16 09:39:32 -0400

    refactor: use `fetch` (remove `got`) and improve some comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import fs from 'node:fs';
 import * as core from '@actions/core';
 import * as cheerio from 'cheerio';
-import got from 'got';
 
 const { hrtime } = process;
 
@@ -52,28 +51,21 @@ export interface Config {
       category: '#richtext-oc8284 > div',
       description: '#richtext-oc7ca0 > div > p',
     },
-    // userAgent: 'Script - Get Current AWS HIPAA Services List',
     jsonFileName: 'index.json',
     jsonOutputDir: 'docs/json/',
     rssFileName: 'feed.rss',
     rssOutputDir: 'docs/rss/',
   };
 
-  // read in previous results to get date for comparison
+  // read in previous results to use for comparison
   const lastResults: ResultsObject = JSON.parse(
     fs.readFileSync(`${config.jsonOutputDir}${config.jsonFileName}`, 'utf-8')
   );
 
-  // make http call to fetch html
-  const markup = await got
-    .get(config.url, {
-      // headers: {
-      //   'User-Agent': config.userAgent,
-      // },
-    })
-    .then((response) => response.body);
+  // make http call to fetch html of updates page
+  const markup = await fetch(config.url).then((response) => response.text());
 
-  // create object to hold results
+  // object to hold JSON items
   const updatesList: ResultsObject = {
     lastUpdated: undefined,
     posts: [],
@@ -82,43 +74,34 @@ export interface Config {
   // instantiate cheerio with markup from earlier http request
   const $ = cheerio.load(markup);
 
-  const rssStart = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-  <channel>
-    <title>Azure Updates Feed</title>
-    <link>https://azure.microsoft.com/en-us/updates/</link>
-    <description>Currently the Azure Updates page has no RSS enabled - this is a temp feed made by scraping that page for updates</description>
-    <language>en-us</language>
-    <atom:link href="https://mikesprague.github.io/azure-updates-feed/rss/feed.rss" rel="self" type="application/rss+xml" />
-    <pubDate>${new Date().toUTCString()}</pubDate>
-`;
-  const rssEnd = `  </channel>
-</rss>
-`;
-
+  // array to hold RSS items
   const rssItems: string[] = [];
 
+  // helper function to encode HTML entities
   const encodeHtmlEntities = (str: string) => {
     return str.replace(/[\u00A0-\u9999<>\&]/gim, (i) => {
       return `&#${i.charCodeAt(0)};`;
     });
   };
 
-  // if date strings are different, proceed with update
-  // loop over services list and add to array in results object
+  // loop over updates and add to array in results object
   $(config.selectors.updateRow).each((i, elem) => {
     let date = $(elem).find(config.selectors.date).text().trim();
     date = `${date}, ${new Date().getFullYear()}`;
+
     let link = $(elem).find(config.selectors.link).attr('href');
     if (link === undefined) {
       link = $(elem).find(config.selectors.linkAlt).attr('href');
     }
+
     const title = $(elem).find(config.selectors.title).text().trim();
     const category = $(elem).find(config.selectors.category).text().trim();
     const description = $(elem)
       .find(config.selectors.description)
       .text()
       .trim();
+
+    // if all data is present, add to results objects
     if (
       date?.length &&
       link?.length &&
@@ -142,6 +125,7 @@ export interface Config {
     </item>`;
       rssItems.push(rssItem);
     } else {
+      // output missing data to run logs
       console.log(
         'Missing data for update:\n',
         `date: ${date}\n`,
@@ -156,10 +140,26 @@ export interface Config {
     }
   });
 
-  // set last updated date string and add to results object
-  updatesList.lastUpdated = new Date().toISOString();
+  // set last updated date string and add it to results object
+  updatesList.lastUpdated = new Date().toUTCString();
 
-  // console.log(updatesList);
+  // var with opening XML for RSS feed
+  const rssStart = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Azure Updates Feed</title>
+    <link>https://azure.microsoft.com/en-us/updates/</link>
+    <description>Currently the Azure Updates page has no RSS enabled - this is a temp feed made by scraping that page for updates</description>
+    <language>en-us</language>
+    <atom:link href="https://mikesprague.github.io/azure-updates-feed/rss/feed.rss" rel="self" type="application/rss+xml" />
+    <pubDate>${updatesList.lastUpdated}</pubDate>
+`;
+
+  // var with closing XML for RSS feed
+  const rssEnd = `  </channel>
+</rss>
+`;
+
   // console.log(rssStart);
   // console.log(rssItems.join('\n'));
   // console.log(rssEnd);
